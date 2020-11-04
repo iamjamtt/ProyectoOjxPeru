2120ee1727a07b24211f5a899bcfe8547853e583
Jamt Mendoza <43344540+pupilojamt@users.noreply.github.com>
2020-11-04 09:51:02 -0500

    actualizaado

--- src/Conexion/ConexionSQL.java
+++ src/Conexion/ConexionSQL.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Conexion;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author jamt_
- */
 public class ConexionSQL {
     public static Connection conexionn() {
         Connection conect = null;
@@ -31,7 +22,7 @@ public class ConexionSQL {
             System.out.println("Error Conexion: "+e.getMessage());
             }  
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null,"Error "+e);
+            JOptionPane.showMessageDialog(null,"Error Error "+e);
         }
         return conect;
     }
